33346855c1163e3c72b25e1f3a3b894713334f83
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-12-31 06:41:11 +0300

    feat: Добавить ApiSpec

---
+++ app/ApiSpec/ApiSpec.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\ApiSpec;
+
+/**
+ * @OA\Info(
+ *     title="Voices of the Void - Void Dispatch API",
+ *     version="1.0.0",
+ *     description="API for Void Dispatch",
+ * )
+ *
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Host URL"
+ * )
+ */
+class ApiSpec {
+
+}
