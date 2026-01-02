6a2ffdc28addf3514d0252de7206e2a77d5f3611
Sazykin Georgii <sazykin.georgii@gmail.com>
2026-01-02 13:17:59 +0300

    refactor: Использовать apiResource вместо отдельных объявлений роутов

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/mails', [MailController::class, 'index']);
-Route::post('/mails', [MailController::class, 'store']);
-Route::patch('/mails/{mail}', [MailController::class, 'update']);
-Route::delete('/mails/{mail}', [MailController::class, 'destroy']);
+Route::apiResource('/mails', MailController::class);
 Route::post('/mails/consume', [MailController::class, 'consume']);
